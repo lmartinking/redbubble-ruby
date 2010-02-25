0b8a944711109cd71a16b2823e9f39026ad06bd2
Lucas Martin-King <lmartinking@gmail.com>
2010-02-25 18:18:17 +1100

    Added Artwork support

--- redbubble.rb
+++ redbubble.rb
@@ -1,5 +1,5 @@
 #--
-#   redbubble-ruby - a Redbubble.com API of sorts	
+#   redbubble-ruby - a Redbubble.com API of sorts, implemented in Ruby	
 #
 #   Copyright (C) 2010  Lucas Martin-King  <lmartinking@gmail.com>
 # 
@@ -21,6 +21,7 @@
 # TODO:
 #  * Add more in the way of error checking
 #  * Callbacks (?)
+#  * Calendar Support
 
 require 'rubygems'
 require 'hpricot'
@@ -46,7 +47,7 @@ class Redbubble
 	end
 
 	class User
-		attr_accessor :username, :img_small, :img_medium, :name, :url
+		attr_accessor :username, :img_small, :img_medium, :name, :url, :groups
 
 		@@sml_sz = '45x45'
 		@@med_sz = '135x135'
@@ -57,7 +58,8 @@ class Redbubble
 			@img_medium = ""
 			@name = ""
 			@url = ""
-			@groups = []
+
+			@groups = UserGroups.new
 		end
 
 		def url
@@ -72,7 +74,6 @@ class Redbubble
 
 		def img_small_refresh
 			@img_small = @img_medium.gsub(@@med_sz, @@sml_sz)			
-			puts "img small: " + @img_small
 			@img_small
 		end
 
@@ -86,18 +87,61 @@ class Redbubble
 				self.img_medium = avatar['src'].to_s
 			}
 
-			#
+			# grab the groups
+			profile.at("#profile-groups").search("a").each {
+				|a|
+				g = UserGroup.new
+				g.name = a.inner_html
+				g.url = Redbubble.domain + a['href']
+				@groups << g
+			}
 		end
 	end
 
+	class UserGroups < Array
+		def initialize
+			super
+		end
+
+		def to_s
+			@gs = []
+			self.each { |g|
+				@gs << g.to_s
+			}
+			str = @gs.join(', ')
+
+			if str == ""
+				return "None"
+			else
+				return str
+			end
+		end
+	end
+
+	class UserGroup
+		attr_accessor :name, :url
+
+		def initialize
+			@name = ""
+			@url = ""
+		end
+
+		def to_s
+			@name
+		end
+	end
+
+
 	class Item
-		attr_accessor :title, :img_small, :img_medium, :img_large, :url, :buy_url, :price, :tags
+		attr_accessor :title, :img_small, :img_medium, :img_large, :url, :buy_url, :price, :tags, :groups, :owner, :forms
 
 		@@nsfw_sml = 'nsfw_small.jpg'
 		@@nsfw_med = 'nsfw_medium.jpg'
 		@@nsfw_lge = 'nsfw_large.jpg'
 
-		def initialize()
+		def initialize
+			@owner = nil
+
 			@title = ""
 			@img_small = ""
 			@img_medium = ""
@@ -105,6 +149,9 @@ class Redbubble
 			@url = ""
 			@buy_url = ""
 			@tags = []
+			@groups = UserGroups.new
+			@forms = []
+			@price = []
 		end
 
 		def to_s
@@ -118,9 +165,119 @@ class Redbubble
 			puts "ImgMed: #{@img_medium}"
 			puts "ImgLge: #{@img_large}"
 		end
+
+		def print_extra
+			puts "Prices: #{@price}"
+			puts "Forms:  #{@forms}"
+			puts "Tags:   #{@tags.to_s}"
+			puts "Groups: #{@groups.to_s}"
+			puts "BuyUrl: #{@buy_url}"
+		end
+
+		def img_small=(url)
+			@img_small = url
+			img_medium_refresh
+			img_large_refresh
+			@img_small
+		end
+
+		def img_medium_refresh
+			nil
+		end
+
+		def img_large_refresh
+			nil
+		end
+
+		def buy_url
+			if @buy_url.empty?
+				extended_info_refresh
+			end
+			@buy_url
+		end
+
+		def price
+			if @price.empty?
+				extended_info_refresh
+			end
+			@price
+		end
+
+		def tags
+			if @tags.empty?
+				extended_info_refresh
+			end
+			@tags
+		end
+
+		def extended_info_refresh
+			nil
+		end
+
+		private
+
+		def item_extended_info_refresh
+			url = @url
+			doc = Redbubble.fetch_page(url)
+			update_price(doc)
+			update_buy_url(doc)
+			update_tags(doc)
+			update_groups(doc)
+			update_forms(doc)
+			return doc
+		end
+
+		def update_tags(doc)
+			# get the tags
+			doc.at("div#tags").search("a").each {
+				|a|
+				@tags << a.inner_html
+			}	
+		end
+
+		def update_groups(doc)
+			doc.at("#description").search("a") {
+				|a|
+				@owner.groups.each {
+					|group|
+					if group.url == Redbubble.domain + a['href']
+						@groups << group	
+					end
+				}
+			}
+		end
+
+		def update_buy_url(doc)
+			# get the buy link
+			doc.at("div#buy").search("a[@title=buy]").each { |a| @buy_url = a['href'].to_s }
+		end
+
+		def update_price(doc)
+			# get the price
+			doc.at("div#buy").search("a").each {
+				|a|
+				price = a.inner_html.scan(/(.*[0-9]+\.[0-9]+)$/)
+
+				if price.empty?
+					next
+				end
+
+				@price << price[0][0].to_s.gsub('from ','')
+			}
+		end
+
+		def update_forms(doc)
+			[ 'T-Shirt:', 'Card:', 'Print:' ].each {
+				|f|
+				result = doc.at("div#buy").inner_html.scan(f)
+				if not result.empty?
+					@forms << result[0].gsub(':','').downcase
+				end
+			}			
+		end
 	end
 
-
+	# Class for Redbuble Clothing
 	class Shirt < Item
 		attr_accessor :color, :colors, :colors_rgb, :gender
 
@@ -129,7 +286,7 @@ class Redbubble
 		@@lge_sz = '550x550' # Unless redbubble changes this
 		@@bgcolor = 'ffffff'		
 
-		def initialize()
+		def initialize
 			super
 
 			@color = ""		# Default colour
@@ -156,6 +313,7 @@ class Redbubble
 			@img_large = @img_small.gsub(@@sml_sz,@@lge_sz).gsub(@@nsfw_sml, @@nsfw_lge)
 		end
 
+
 		def nsfw?
 			@img_small.include? @@nsfw_sml
 		end
@@ -174,67 +332,45 @@ class Redbubble
 			@colors
 		end
 
-		def buy_url
-			if @buy_url.empty?
-				extended_info_refresh
-			end
-			@buy_url
-		end
-
-		def price
-			if @price.empty?
-				extended_info_refresh
-			end
-			@price
-		end
-
-		def tags
-			if @tags.empty?
-				extended_info_refresh
-			end
-			@tags
-		end
-
 		def extended_info_refresh
-			url = @url
-			doc = Redbubble.fetch_page(url)
-
-			# get the buy link
-			doc.at("div#buy").search("a[@title=buy]").each { |a| @buy_url = a['href'].to_s }
-
-			# get the price
-			doc.at("div#buy").search("a").each {
-				|a|
-				if a.inner_html.match('from ')
-					price = a.inner_html.scan(/from (.*[0-9]+\.[0-9]+)$/)
-					@price = price[0][0] # XXX: why? break out from regex 
-				end
-			}
+			doc = item_extended_info_refresh
 
 			# get the shirt colour options
 			doc.search("ul#color-options").each {
 				|element|
 				element.search("a").each {
 					|this|
-					@colors << this['title'].to_s.downcase
+					@colors << this.at("span").inner_html.to_s.downcase
 					style = this['style'].to_s
 					rgb = style.scan(/background: (#.+);/)[0][0]
-					@colors_rgb << rgb.to_s
+					@colors_rgb << rgb.to_s.downcase
 				}
-			}
-
-			# get the tags
-			doc.at("div#tags").search("a").each {
-				|a|
-				@tags << a.inner_html
 			}
 		end
 	end
 
-
+	# Class for Redbuble Artwork
 	class Artwork < Item
+		@@sml_sz = '135x135'
+		@@med_sz = '550x550'
+		@@lge_sz = '800x800'
+		
 		def initialize()
 			super
+		end
+
+		def img_medium_refresh
+			# Our clever little dodad to figure out the larger size	
+			@img_medium = @img_small.gsub(/fc/,'ts').gsub(@@sml_sz,@@med_sz).gsub(@@nsfw_sml, @@nsfw_med)	
+		end
+
+		def img_large_refresh
+			# Our clever little dodad to figure out the larger size
+			@img_large = @img_small.gsub(@@sml_sz,@@lge_sz).gsub(@@nsfw_sml, @@nsfw_lge)
+		end
+
+		def extended_info_refresh
+			doc = item_extended_info_refresh
 		end
 	end
 
@@ -251,7 +387,8 @@ class Redbubble
 			works.map.each {
 				|item|
 			
-				s = Shirt.new()
+				s = Shirt.new
+				s.owner = @user
 
 				s.url = @@domain + item.at("a")['href'].to_s
 				s.title = item.at("span.title").at("a")['title'].to_s
@@ -263,7 +400,25 @@ class Redbubble
 	end
 
 	def init_artworks
-		puts "Not yet implemented!"
+		pages = gather_pages('art')
+
+		pages.each {
+			|doc|
+
+			works = doc.search("span.work-info")
+			works.map.each {
+				|item|
+			
+				a = Artwork.new
+				a.owner = @user
+
+				a.url = @@domain + item.at("a")['href'].to_s
+				a.title = item.at("span.title").at("a")['title'].to_s
+				a.img_small = item.at("img")['src'].to_s
+
+				@artworks << a
+			}
+		}					
 	end
 
 	def shirts
