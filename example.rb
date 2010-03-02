f8c76effe23bd5ea7a616c28593940297445f833
Lucas Martin-King <lmartinking@gmail.com>
2010-03-02 11:52:24 +1100

    Changed example.rb a bit, to show more info but not fetch everything

--- example.rb
+++ example.rb
@@ -4,23 +4,52 @@ require 'redbubble'
 
 rb = Redbubble.new('lmartinking')
 
+rb.custom_fetch = proc {
+	|url|
+	puts "Opening... #{url}"
+	open(url)
+}
+
+
+rb.user.extended_info_refresh
+
+puts
+puts "Username: " + rb.user.username
+puts "Name:     " + rb.user.name
+puts "ImgSml:   " + rb.user.img_small
+puts "ImgMed:   " + rb.user.img_medium
+puts "Groups:   " + rb.user.groups.to_s
+puts "URL:      " + rb.user.url
+puts
+
+
+artworks = rb.artworks
+
+artworks.each {
+	|art|
+
+	art.print
+#	art.print_extra
+	puts
+}
+
+
 shirts = rb.shirts
 
 shirts.each {
 	|shirt|
-	
+
 	shirt.print
+#	shirt.print_extra
 
-	if shirt.nsfw?
-		puts "NSFW!!!"
-	else
-		puts shirt.colors.to_s
-		puts shirt.colors_rgb.to_s
-	end
 
-	puts "buy url: " + shirt.buy_url.to_s
-	puts "price:   " + shirt.price.to_s
-	puts "tags:    " + shirt.tags.to_s
+#	if shirt.nsfw?
+#		puts "NSFW!!!"
+#	else
+#		puts shirt.colors.to_s
+#		puts shirt.colors_rgb.to_s
+#	end
+
 	puts
 }
 
